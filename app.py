cdf58861c4d7e469d2dbb0024654b644f8c693c3
learner027351 <vineetsingh27351@gmail.com>
2025-12-16 20:33:04 +0530

    adding nltk download to be added on render

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer 
 import string
 
+
+
+
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
 ps=PorterStemmer()
 
 def transform_text(text):
